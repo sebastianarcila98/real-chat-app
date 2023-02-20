2629874405b13025da4c5ca9c98476a49105a5fc
Sebastian Arcila <sarcila1998@gmail.com>
2023-02-20 17:35:43 -0500

    added firestore package and established listener with firestore datbase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -7,9 +8,26 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Chat')),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => Container(child: Text('hey')),
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection('chats/ui4GSrapowtTkHJIyJXj/messages')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          final documents = snapshot.data.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (context, index) => Container(
+              child: Text(documents[index]['text']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
       ),
     );
   }
